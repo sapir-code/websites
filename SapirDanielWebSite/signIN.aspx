8f58ce70255060aef6f8f1a24c56dbb00e4ae5b1
Sapir <sapir.daniel1010@gmail.com>
2026-01-27 16:32:10 +0200

    טופס הרשמה "מאחורי הקלעים - עדיין לא עשיתי את הטופס עצמו

--- SapirDanielWebSite/signIN.aspx
+++ SapirDanielWebSite/signIN.aspx
@@ -4,5 +4,7 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <h1> כניסה</h1>
+
+    <% =stResult %>
 </asp:Content>
 
--- SapirDanielWebSite/signIN.aspx.cs
+++ SapirDanielWebSite/signIN.aspx.cs
@@ -7,8 +7,29 @@ using System.Web.UI.WebControls;
 
 public partial class signIN : System.Web.UI.Page
 {
+    public string stResult = "";
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Page.IsPostBack)
+        {
 
+            string email = Request.Form["email"];
+            string password = Request.Form["password"];
+
+
+             string sqSelect =
+                "SELECT * FROM tUsers " +
+                "WHERE Email = N' " + email + "' " + 
+                "AND UserPassword = N'" + password + "'";
+
+
+            bool userExists = MyAdoHelper.IsExist(sqSelect);
+
+            if (!userExists)
+                stResult = "אימייל או סיסמה שגויים";
+            else
+                stResult = " משתמש רשום";
+
+            }
     }
 }
