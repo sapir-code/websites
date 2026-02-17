5b7426fbbcce09206a4ed43f73f9e57238fa46e7
sapir-code <sapir.daniel1010@gmail.com>
2026-02-17 14:58:41 +0200

    טופס מנהל

--- SapirDanielWebSite/manege.aspx
+++ SapirDanielWebSite/manege.aspx
@@ -3,6 +3,26 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    <h1> דף ניהול </h1>
+    <h1> דף מנהל </h1>
+
+
+    <form name="managerForm" method="post" runat="server">
+        <table>
+            <tr>
+                <td>  שם מלא </td>
+                <input type="text" name="fullname" id="fullname" placeholder="רשום שם">   
+            </tr>  
+    <br />
+             <td>  אימייל </td>
+    <input type="text" name="email" id="email" placeholder="example@gmail.com">
+
+         <input type="submit" value="שלח" />
+            
+        </table> 
+</form>
+
+    <% = st %>
+    <br />
+
 </asp:Content>
 
--- SapirDanielWebSite/manege.aspx.cs
+++ SapirDanielWebSite/manege.aspx.cs
@@ -4,11 +4,65 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class manege : System.Web.UI.Page
 {
+
+    public string st = ""; // <table>
+    public string fullname;
+    public string email;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        if (IsPostBack)
+        {
+            string fullname = Request.Form["fullname"];
+            string email = Request.Form["email"];
+
+            // אוסף את כל הרשומות
+            string sql = "SELECT * FROM  tUsers  WHERE" +
+              " email = N'" + email + "' AND " +
+               "fullname = N'" + fullname + "'";
+
+
+            DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
+
+            if (dt.Rows.Count == 0)
+            {
+                st = " אין נתונים";
+            }
+            else
+            {
+                st += "<table border='1'>";
+                st += "<tr >";
+                st += "<th> שם מלא </th>";
+                st += "<th> מייל </th>";
+                st += "<th> סיסמה </th>";
+                st += "<th> פתקים </th>";
+                st += "</tr>";
+
+            }
+
+
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                st += "<tr>";
+
+                for (int k = 0; k < dt.Columns.Count; k++)
+                {
+                    st += "<td>";
+                    st += dt.Rows[i][k];
+                    st += "</td>";
+                }
+                st += "</tr>";
+            }
+
+            st += "</table> ";
+        }
     }
 }
+
+
